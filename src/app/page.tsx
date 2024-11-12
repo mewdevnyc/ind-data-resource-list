aaf407731595a9879e351d9034122ef419882dbf
MEW <contactmeganwalsh@gmail.com>
2024-11-12 15:30:07 -0500

    refactor search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { prisma } from '../../lib/prisma';
 import ResourceCard from '@/components/ResourceCard';
+import SearchInput from '@/components/SearchInput';
+
 export default async function Home() {
 	const resources = await prisma.resource.findMany();
-	console.log(resources);
+
 	return (
 		<main className="">
 			<ul>
+				<SearchInput />
 				{resources?.map(resource => (
 					<ResourceCard key={resource.id} {...resource} />
 				))}
