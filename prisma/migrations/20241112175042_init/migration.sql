293b1f052b7dfb87848669fbacfae4e727e0da31
MEW <contactmeganwalsh@gmail.com>
2024-11-12 15:31:09 -0500

    delete old migration files

--- prisma/migrations/20241112175042_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `type` to the `Resource` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Resource" ADD COLUMN     "type" TEXT NOT NULL;
