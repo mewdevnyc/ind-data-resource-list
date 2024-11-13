623294ddeed59870e6a02d378620a2434210feb5
MEW <contactmeganwalsh@gmail.com>
2024-11-13 10:02:52 -0500

    removed resource list render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
-import { db } from '../../lib/prisma';
-import ResourceCard from '@/components/ResourceCard';
 import SearchInput from '@/components/SearchInput';
 
 export default async function Home() {
-	const resources = await db.resource.findMany();
-
 	return (
 		<main className="">
 			<ul>
 				<SearchInput />
-				{resources?.map(resource => (
-					<ResourceCard key={resource.id} {...resource} />
-				))}
 			</ul>
 		</main>
 	);
