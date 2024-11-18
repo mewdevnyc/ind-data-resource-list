b46b64476f6de72a83ea252ff3cd13b2b002fb82
MEW <contactmeganwalsh@gmail.com>
2024-11-18 15:55:34 -0500

    homepage displays all available resources in databse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
-import SearchInput from '@/components/SearchInput';
-
-export default async function Home() {
+'use client';
+import ResourceCard from '@/components/ResourceCard';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { Resource } from '@prisma/client';
+export default function Home() {
+	const { data: resourceTags, isLoading: isLoadingTags } = useQuery<Resource[]>(
+		{
+			queryKey: ['resources'],
+			queryFn: async () => {
+				const response = await axios.get('/api/resources');
+				return response.data;
+			},
+		}
+	);
+	console.log(resourceTags);
 	return (
 		<main className="">
-			<ul>
-				<SearchInput />
-			</ul>
+			{isLoadingTags ? (
+				'Loading...'
+			) : (
+				<div>
+					{resourceTags?.map(resource => (
+						<ResourceCard key={resource.id} {...resource} />
+					))}
+				</div>
+			)}
 		</main>
 	);
 }
