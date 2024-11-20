c9c62105c6429aa3e91bc214e557d6e7080b2046
MEW <contactmeganwalsh@gmail.com>
2024-11-19 19:40:35 -0500

    did something

--- src/app/api/resources/route.ts
+++ src/app/api/resources/route.ts
@@ -3,7 +3,11 @@ import { db } from '../../../../lib/prisma';
 
 export async function GET() {
 	try {
-		const resources = await db.resource.findMany();
+		const resources = await db.resource.findMany({
+			include: {
+				trainings: true,
+			},
+		});
 		return NextResponse.json(resources, { status: 200 });
 	} catch (error) {
 		return NextResponse.json(
