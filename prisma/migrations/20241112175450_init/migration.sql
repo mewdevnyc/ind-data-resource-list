67bc4340b439e8b60f38e19be16f7b3744cf5e76
MEW <contactmeganwalsh@gmail.com>
2024-11-19 23:25:00 -0500

    deleted migration file - new schema

--- prisma/migrations/20241112175450_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `type` on the `Resource` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Resource" DROP COLUMN "type";
