132d8872807c979d1ba47dc561fdd00edc914515
MEW <contactmeganwalsh@gmail.com>
2024-12-04 15:34:01 -0500

    layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import ResourceCard from '@/components/ResourceCard';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { Prisma } from '@prisma/client';
+import { Section } from '@radix-ui/themes';
+
 type ResourceWithRelations = Prisma.ResourceGetPayload<{
 	include: {
 		trainings: true;
@@ -22,19 +24,25 @@ export default function Home() {
 	});
 	console.log(resourceTags);
 	return (
-		<section className="">
-			<h2 className="">Resources</h2>
-			<div className="grid grid-cols-1 gap-4">
-				{isLoadingTags ? (
-					'Loading...'
-				) : (
-					<>
-						{resourceTags?.map(resource => (
-							<ResourceCard key={resource.id} {...resource} />
-						))}
-					</>
-				)}
-			</div>
-		</section>
+		<>
+			<Section id="" size="2">
+				<p>TEXT</p>
+			</Section>
+
+			<section id="resources" className="">
+				<h2 className="">Resources</h2>
+				<div className="grid grid-cols-1 gap-4">
+					{isLoadingTags ? (
+						'Loading...'
+					) : (
+						<>
+							{resourceTags?.map(resource => (
+								<ResourceCard key={resource.id} {...resource} />
+							))}
+						</>
+					)}
+				</div>
+			</section>
+		</>
 	);
 }
