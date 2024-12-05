a6209c4da657ff7f92fe98a0436d826556c57399
MEW <contactmeganwalsh@gmail.com>
2024-12-05 12:14:15 -0500

    remove development migrations

--- prisma/migrations/20241120065646_init2/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Contact" ALTER COLUMN "title" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Resource" ALTER COLUMN "description" SET DEFAULT '';
-
--- AlterTable
-ALTER TABLE "Training" ALTER COLUMN "description" SET DEFAULT '';
